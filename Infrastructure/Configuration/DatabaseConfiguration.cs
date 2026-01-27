d7ce67909aca07874e7cfaccb8f8230a2062adee
khetz <vukheta99@gmail.com>
2026-01-27 21:43:33 +0200

    Added database configuration class

---
+++ Infrastructure/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace Infrastructure.Configuration
+{
+    public class DatabaseConfiguration
+    {
+        public const string NotesDatabase = "NotesDatabase";
+        public required string ConnectionString { get; set; }
+    }
+}
