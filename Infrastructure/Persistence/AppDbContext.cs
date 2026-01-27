fb0a3699f756e7bd5d93349fca76d0bc0cb62f42
khetz <vukheta99@gmail.com>
2026-01-27 21:54:10 +0200

    Added db context and configured database and connection string

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContext :  DbContext
+    {
+        public required DbSet<Note> Notes { get; set; }
+        public required DbSet<User> Users { get; set; }
+        public required DbSet<Category> Categories { get; set; }
+
+        private readonly DatabaseConfiguration _databaseConfiguration;
+
+        public AppDbContext(IOptions<DatabaseConfiguration> databaseConfiguration)
+        {
+            _databaseConfiguration = databaseConfiguration.Value;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_databaseConfiguration.ConnectionString);
+        }
+    }
+}
