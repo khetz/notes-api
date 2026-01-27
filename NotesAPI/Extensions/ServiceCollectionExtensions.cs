3a1a8a9e088c1b89e1a3cc44f343dc050b69410d
khetz <vukheta99@gmail.com>
2026-01-27 21:55:20 +0200

    Added service collection extensions to neaten registration process

---
+++ NotesAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Configuration;
+using Infrastructure.Persistence;
+
+namespace NotesAPI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<DatabaseConfiguration>(
+                configuration.GetSection(DatabaseConfiguration.NotesDatabase));
+
+            return services;
+        }
+
+        public static IServiceCollection RegisterDbContexts(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>();
+            return services;
+        }
+    }
+}
