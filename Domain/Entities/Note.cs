ef8426e53edd6316a37d87b4f9caab0e46d3b8e6
khetz <vukheta99@gmail.com>
2026-01-27 21:37:13 +0200

    Added creation and update tracking fields to note entity

--- Domain/Entities/Note.cs
+++ Domain/Entities/Note.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Content { get; set; }
-        public required DateTime CreationDateTime { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime LastUpdatedAt { get; set; }
 
         public int CategoryId { get; set; }
         public required Category Category { get; set; }
