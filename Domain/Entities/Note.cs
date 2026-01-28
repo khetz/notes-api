14b0ac90d44e27cfd83c4dd5e06623228cf3e4fc
khetz <vukheta99@gmail.com>
2026-01-28 21:39:18 +0200

    Added order field to note class

--- Domain/Entities/Note.cs
+++ Domain/Entities/Note.cs
@@ -7,6 +7,7 @@
         public required string Content { get; set; }
         public required DateTime CreatedAt { get; set; }
         public required DateTime LastUpdatedAt { get; set; }
+        public required int Order {  get; set; }
 
         public int CategoryId { get; set; }
         public required Category Category { get; set; }
