bbffc99b36ae277e90d177164200b48e29de6f6a
khetz <vukheta99@gmail.com>
2026-01-28 21:24:17 +0200

    Simplified db context in the interim

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,26 +1,12 @@
 ﻿using Domain.Entities;
-using Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Persistence
 {
-    public class AppDbContext :  DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) :  DbContext(options)
     {
-        public required DbSet<Note> Notes { get; set; }
-        public required DbSet<User> Users { get; set; }
-        public required DbSet<Category> Categories { get; set; }
-
-        private readonly DatabaseConfiguration _databaseConfiguration;
-
-        public AppDbContext(IOptions<DatabaseConfiguration> databaseConfiguration)
-        {
-            _databaseConfiguration = databaseConfiguration.Value;
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(_databaseConfiguration.ConnectionString);
-        }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
