f82e634b9ba68a028d6d0f0d0e4e199092b0895f
khetz <vukheta99@gmail.com>
2026-01-28 22:13:17 +0200

    added dummy endpoint to test app

---
+++ NotesAPI/Endpoints/NoteEndpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace NotesAPI.Endpoints
+{
+    public static class NoteEndpoints
+    {
+        public static void MapNoteEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("notes");
+
+            group.MapGet("", Handler);
+        }
+
+        private static Task Handler()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
