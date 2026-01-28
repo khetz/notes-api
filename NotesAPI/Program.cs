f0c606da45e5dfa8163492b4ebc8f90f25c9988e
khetz <vukheta99@gmail.com>
2026-01-28 22:12:49 +0200

    Added swagger configurations

--- NotesAPI/Program.cs
+++ NotesAPI/Program.cs
@@ -1,3 +1,4 @@
+using NotesAPI.Endpoints;
 using NotesAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services
 builder.Services
     .AddConfig(builder.Configuration)
-    .RegisterDbContexts();
+    .RegisterDbContexts(builder.Configuration);
+
+// OpenAPI/Swagger config
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapNoteEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
 app.Run ();
