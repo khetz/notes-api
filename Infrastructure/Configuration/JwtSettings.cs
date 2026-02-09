1dafc6802fe3df2cb030e5584083be23c7005763
khetz <vukheta99@gmail.com>
2026-02-09 20:56:15 +0200

    Added jwt settings config class

---
+++ Infrastructure/Configuration/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Configuration;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+    public required string Key { get; set; }
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public required int ExpiryMinutes { get; set; }
+}
