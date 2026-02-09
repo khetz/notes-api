e02c0f15aca49382285e6f7f485a52d40dea314e
khetz <vukheta99@gmail.com>
2026-02-09 20:53:18 +0200

    Removed unused import

--- Infrastructure/Configuration/DatabaseConfiguration.cs
+++ Infrastructure/Configuration/DatabaseConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
-
-namespace Infrastructure.Configuration
+﻿namespace Infrastructure.Configuration
 {
     public class DatabaseConfiguration
     {
