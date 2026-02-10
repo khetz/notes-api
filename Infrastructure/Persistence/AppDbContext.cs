541b1d42828d16cfb7b6c5c0803c6f66ee61db35
khetz <vukheta99@gmail.com>
2026-02-10 21:42:49 +0200

    Restored db context structure

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,28 +1,12 @@
 ﻿using Domain.Entities;
-using Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Persistence
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Note> Notes { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
-
-        private readonly DatabaseConfiguration _databaseConfiguration;
-
-        public AppDbContext(DbContextOptions<AppDbContext> options, IOptions<DatabaseConfiguration> databaseConfiguration)
-            : base(options)
-        {
-            _databaseConfiguration = databaseConfiguration.Value;
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-                optionsBuilder.UseSqlServer(_databaseConfiguration.ConnectionString);
-        }
     }
 }
