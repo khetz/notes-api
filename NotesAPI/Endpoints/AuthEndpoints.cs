969208259bdc21ea5f62c731782d80e3b886dc97
khetz <vukheta99@gmail.com>
2026-02-10 07:09:58 +0200

    Added login request and, used it in initial login endpoint setup

--- NotesAPI/Endpoints/AuthEndpoints.cs
+++ NotesAPI/Endpoints/AuthEndpoints.cs
@@ -1,4 +1,8 @@
-﻿namespace NotesAPI.Endpoints
+﻿using Infrastructure.Security;
+using Microsoft.AspNetCore.Mvc;
+using NotesAPI.Requests;
+
+namespace NotesAPI.Endpoints
 {
     public static class AuthEndpoints
     {
@@ -6,7 +10,19 @@
         {
             var authGroup = group.MapGroup("auth");
 
+            authGroup.MapPost("login", LoginHandler);
+        }
 
+        private static IResult LoginHandler([FromBody] LoginRequest loginRequest, [FromServices] JwtService jwtService)
+        {
+            // fetch hashed password
+            var storedHashedPassword = "$hibrgjkb";
+
+            if (!PasswordHashingService.VerifyPassword(loginRequest.Password, storedHashedPassword))
+                return Results.Unauthorized();
+
+            var token = jwtService.GenerateToken(1, loginRequest.Username);
+            return Results.Ok(token);
         }
     }
 }
