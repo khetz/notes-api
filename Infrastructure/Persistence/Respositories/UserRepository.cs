1308866a9536c66b7dde1c90e7d53d8f82818fe2
khetz <vukheta99@gmail.com>
2026-02-10 21:01:13 +0200

    User repository and service implementations for user addition

---
+++ Infrastructure/Persistence/Respositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Respositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Infrastructure/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Inputs;
+using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Security;
+
+namespace Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task RegisterUserAsync(RegisterUserRequest registrationRequest)
+        {
+            var user = new User
+            {
+                Username = registrationRequest.Username,
+                PasswordHash = PasswordHashingService.HashPassword(registrationRequest.Password)
+            };
+
+            await _userRepository.AddAsync(user);
+        }
+    }
+}
