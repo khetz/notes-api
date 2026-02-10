f5e61222aa37a56a21a5fe7c27a25b91e41f6135
khetz <vukheta99@gmail.com>
2026-02-10 21:27:22 +0200

    Added user mapper class

---
+++ NotesAPI/Mappers/UserMappers.cs
@@ -0,0 +1,13 @@
+﻿using NotesAPI.Requests;
+using ApplicationRegisterUserRequest = Application.Inputs.RegisterUserRequest;
+
+namespace NotesAPI.Mappers;
+
+internal static class UserMappers
+{
+    internal static ApplicationRegisterUserRequest ToApplicationRegisterUserRequest(this RegisterUserRequest registerUserRequest) => new()
+    { 
+        Username = registerUserRequest.Username,
+        Password = registerUserRequest.Password
+    };
+}
