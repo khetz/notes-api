92fbc8b558ecdce515ea9a1ed1ab15fbbced05f9
khetz <vukheta99@gmail.com>
2026-02-10 20:59:53 +0200

    Added user registration requests to API and Application

---
+++ Application/Inputs/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Inputs
+{
+    public class RegisterUserRequest
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ NotesAPI/Requests/RegisterUserRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace NotesAPI.Requests;
+
+public class RegisterUserRequest
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
