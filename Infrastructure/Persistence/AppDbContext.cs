b52161b0e60b70cdf2d1d2e93cc3c9fe1f22bd66
khetz <vukheta99@gmail.com>
2026-02-10 21:26:59 +0200

    Changed db context constructor format and specified runtime connection string

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,12 +1,28 @@
 ﻿using Domain.Entities;
+using Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Persistence
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) :  DbContext(options)
+    public class AppDbContext : DbContext
     {
         public DbSet<Note> Notes { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        private readonly DatabaseConfiguration _databaseConfiguration;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options, IOptions<DatabaseConfiguration> databaseConfiguration)
+            : base(options)
+        {
+            _databaseConfiguration = databaseConfiguration.Value;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(_databaseConfiguration.ConnectionString);
+        }
     }
 }
