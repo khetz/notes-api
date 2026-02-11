53120123f23ba03f274bf36a285dcb59f1eed92c
khetz <vukheta99@gmail.com>
2026-02-11 20:04:09 +0200

    Simple endpoints setup to test authorization

--- NotesAPI/Endpoints/NoteEndpoints.cs
+++ NotesAPI/Endpoints/NoteEndpoints.cs
@@ -1,16 +1,19 @@
-﻿namespace NotesAPI.Endpoints
+﻿using System.Security.Claims;
+
+namespace NotesAPI.Endpoints
 {
     public static class NoteEndpoints
     {
         public static void MapNoteEndpoints(this RouteGroupBuilder group)
         {
-            var notesGroup = group.MapGroup("notes");
+            var notesGroup = group.MapGroup("notes").RequireAuthorization();
 
             notesGroup.MapGet("", Handler);
         }
 
-        private static Task Handler()
+        private static Task Handler(ClaimsPrincipal user)
         {
+            var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
             return Task.CompletedTask;
         }
     }
