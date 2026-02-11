3e27ac855d3d65eae666511827499da33cec750d
khetz <vukheta99@gmail.com>
2026-02-11 18:59:50 +0200

    Updated db context models to db models

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,12 +1,12 @@
-﻿using Domain.Entities;
+﻿using Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public DbSet<Note> Notes { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<NoteDbModel> Notes { get; set; }
+        public DbSet<UserDbModel> Users { get; set; }
+        public DbSet<CategoryDbModel> Categories { get; set; }
     }
 }
