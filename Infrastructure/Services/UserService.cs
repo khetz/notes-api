1299beebbdf8586a01d9746f87c11bee1ad797cb
khetz <vukheta99@gmail.com>
2026-02-18 21:05:30 +0200

    Removed unused imports and services in user service

--- Infrastructure/Services/UserService.cs
+++ Infrastructure/Services/UserService.cs
@@ -1,22 +1,17 @@
-﻿using Application.Inputs;
-using Application.Interfaces;
+﻿using Application.Interfaces;
 using Application.Services;
 using Domain.Entities;
 using ErrorOr;
-using Infrastructure.Security;
-using Microsoft.AspNetCore.Identity.Data;
 
 namespace Infrastructure.Services
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
-        private readonly JwtService _jwtService;
-
-        public UserService(IUserRepository userRepository, JwtService jwtService)
+        
+        public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
-            _jwtService = jwtService;
         }
 
         public async Task<ErrorOr<User>> GetByUsernameAsync(string username)
