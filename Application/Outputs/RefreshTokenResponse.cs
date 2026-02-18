f5526f4f534de4e0ce224e15c931985a693f6397
khetz <vukheta99@gmail.com>
2026-02-18 22:11:24 +0200

    Changed refresh token response to include access and refresh tokens

--- Application/Outputs/RefreshTokenResponse.cs
+++ Application/Outputs/RefreshTokenResponse.cs
@@ -2,8 +2,7 @@
 {
     public class RefreshTokenResponse
     {
-        public required string Token { get; set; }
-        public required string Username { get; set; }
-        public DateTime ExpirationDate { get; set; }
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
     }
 }
