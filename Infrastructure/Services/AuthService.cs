958fe725721564e4c086bceae856c365758422e9
khetz <vukheta99@gmail.com>
2026-02-18 21:13:43 +0200

    Implemented login and register functions in auth service and, started refresh function

--- Infrastructure/Services/AuthService.cs
+++ Infrastructure/Services/AuthService.cs
@@ -1,15 +1,47 @@
 ﻿using Application.Inputs;
 using Application.Outputs;
 using Application.Services;
+using Domain.Entities;
 using ErrorOr;
+using Infrastructure.Security;
 
 namespace Infrastructure.Services
 {
     public class AuthService : IAuthService
     {
-        public Task<ErrorOr<string>> LoginAsync(LoginRequest loginRequest)
+        private readonly JwtService _jwtService;
+        private readonly IUserService _userService;
+
+        public AuthService(JwtService jwtService, IUserService userService)
         {
-            throw new NotImplementedException();
+            _jwtService = jwtService;
+            _userService = userService;
+        }
+
+        public async Task<ErrorOr<string>> LoginAsync(LoginRequest loginRequest)
+        {
+            var user = await _userService.GetByUsernameAsync(loginRequest.Username);
+
+            if (user.IsError) return user.FirstError;
+
+            var storedHashedPassword = user.Value.PasswordHash;
+
+            if (!PasswordHashingService.VerifyPassword(loginRequest.Password, storedHashedPassword))
+                return Error.Unauthorized();
+
+            var token = _jwtService.GenerateToken(user.Value.Id, loginRequest.Username);
+            return token;
+        }
+
+        public async Task RegisterUserAsync(RegisterUserRequest registrationRequest)
+        {
+            var user = new User
+            {
+                Username = registrationRequest.Username,
+                PasswordHash = PasswordHashingService.HashPassword(registrationRequest.Password)
+            };
+
+            await _userService.AddUserAsync(user);
         }
 
         public Task<ErrorOr<RefreshTokenResponse>> RefreshAsync(string refreshToken)
@@ -17,9 +49,5 @@ namespace Infrastructure.Services
             throw new NotImplementedException();
         }
 
-        public Task RegisterUserAsync(RegisterUserRequest registrationRequest)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
