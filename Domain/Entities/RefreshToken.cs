2039f97a46eed206fe897b20d41c388010fc8c86
khetz <vukheta99@gmail.com>
2026-02-19 21:53:35 +0200

    Added refresh token migration/table

--- Domain/Entities/RefreshToken.cs
+++ Domain/Entities/RefreshToken.cs
@@ -2,6 +2,7 @@
 {
     public class RefreshToken
     {
+        public int Id { get; set; }
         public required string Token { get; set; }
         public required string Username { get; set; }
         public DateTime ExpirationDate { get; set; }
