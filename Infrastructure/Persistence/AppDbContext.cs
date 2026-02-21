f07ee7a73e6d1a16b25c073e02ee9959310e1068
khetz <vukheta99@gmail.com>
2026-02-21 15:47:42 +0200

    Removed redundant db models

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,14 +1,13 @@
 ﻿using Domain.Entities;
-using Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public DbSet<NoteDbModel> Notes { get; set; }
-        public DbSet<UserDbModel> Users { get; set; }
-        public DbSet<CategoryDbModel> Categories { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
