82e0507bfc7e08e4a284b656af9fa483a973f65c
khetz <vukheta99@gmail.com>
2026-02-21 15:54:56 +0200

    Removed required modifiers and made category nullable

--- Domain/Entities/Note.cs
+++ Domain/Entities/Note.cs
@@ -5,11 +5,11 @@
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Content { get; set; }
-        public required DateTime CreatedAt { get; set; }
-        public required DateTime LastUpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
         public required int Order {  get; set; }
 
-        public int CategoryId { get; set; }
-        public required Category Category { get; set; }
+        public required int CategoryId { get; set; }
+        public Category? Category { get; set; }
     }
 }
