e1931b35605311bd6792b947acc4d3396232b76e
khetz <vukheta99@gmail.com>
2026-02-21 17:25:03 +0200

    Added category creation and modification times

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
 
         public ICollection<Note> Notes { get; set; } = [];
         public int UserId { get; set; }
