92bbdf286bfb7972bcc5de177e6f926c9139e0e8
khetz <vukheta99@gmail.com>
2026-02-21 16:14:36 +0200

    Added mapper function to aid note creation

---
+++ Infrastructure/Mappers/NoteMappers.cs
@@ -0,0 +1,19 @@
+﻿using Application.Inputs;
+using Domain.Entities;
+
+namespace Infrastructure.Mappers
+{
+    internal static class NoteMappers
+    {
+        internal static Note ToNote(this CreateNoteRequest request, int userId) => new()
+        {
+            Title = request.Title,
+            Content = request.Content,
+            CategoryId = request.CategoryId,
+            Category = null,
+            UserId = userId,
+            User = null,
+            Order = 0
+        };
+    }
+}
