023c743cc717dbcf42fc5911a0f2bf303ea6170c
khetz <vukheta99@gmail.com>
2026-02-21 15:51:55 +0200

    Added note creation request

---
+++ Application/Inputs/CreateNoteRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Inputs;
+
+public class CreateNoteRequest
+{
+    public required string Title { get; set; }
+    public required string Content { get; set; }
+    public required int CategoryId { get; set; }
+}
